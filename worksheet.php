314475ce24ba65141da58fe1d162f0864147881a
mlee432 <leepogii@gmail.com>
2017-06-12 18:18:35 -0400

    Add sorting table

--- worksheet.php
+++ worksheet.php
@@ -60,108 +60,119 @@
                     if ($conn->connect_error) {
                         die("Connection failed: " . $conn->connect_error);
                     }
-                    $sql = "SELECT * FROM Worksheet";
-                    $result = mysqli_query($conn, $sql);
+
                     if ($_SESSION['isadmin']) {
+                        echo '
+                            <table border="2" width="1000">
+                                <thead>
+                                    <tr>
+                                        <td align="center"><a href=\"?orderBy=invoice\">Invoice #</a></td>
+                                        <td align="center"><a href=\"?orderBy=po\">P.O. #</a></td>
+                                        <td align="center"><a href=\"?orderBy=apt\">Apt #</a></td>
+                                        <td align="center"><a href=\"?orderBy=unit\">Unit #</a></td>
+                                        <td align="center"><a href=\"?orderBy=size\">Size</a></td>
+                                        <td align="center"><a href=\"?orderBy=price\">Price</a></td>
+                                        <td align="center"><b>Descrition</b></td>
+                                        <td align="center"><a href=\"?orderBy=date\">Date</a></td>
+                                    </tr>
+                                </thead>';
+                        $orderBy = array('invoice', 'po', 'apt', 'unit', 'size', 'price', 'date');
+                        $order = 'date';
+                        if (isset($_GET['orderBy']) && in_array($_GET['orderBy'], $orderBy)) {
+                            $order = $_GET['orderBy'];
+                        }
                         $sql = 'SELECT * FROM Worksheet ORDER BY '.$order;
                         $result = mysqli_query($conn, $sql);
-                        echo '
-                        <table border="2" width="1000">
-                            <thead>
-                                <tr>
-                                    <td align="center"><b>Invoice #</b></td>
-                                    <td align="center"><b>P.O. #</b></td>
-                                    <td align="center"><b>Apt #</b></td>
-                                    <td align="center"><b>Unit #</b></td>
-                                    <td align="center"><b>Size</b></td>
-                                    <td align="center"><b>Price</b></td>
-                                    <td align="center"><b>Descrition</b></td>
-                                    <td align="center"><b>Date</b></td>
-                                </tr>
-                            </thead>';
-
-                            while($row = mysqli_fetch_array($result))
-                            {
-                                echo '
-                                    <tbody>
-                                        <tr>
-                                            <td align="center">'.$row['invoice'].'</td>
-                                            <td align="center">'.$row['po'].'</td>
-                                            <td align="center">'.$row['apt'].'</td>
-                                            <td align="center">'.$row['unit'].'</td>
-                                            <td align="center">'.$row['size'].'</td>
-                                            <td align="center">'.$row['price'].'</td>
-                                            <td align="center">'.$row['description'].'</td>
-                                            <td align="center">'.$row['date'].'</td>
-                                            <td align="center"><input type="submit" value="Send" onclick="location.href=\'assign.php\'"></input></td>
-                                        </tr>
-                                    </tbody>';
-                            }
+                        while($row = mysqli_fetch_array($result))
+                        {
+                            echo '
+                                <tbody>
+                                    <tr>
+                                        <td align="center">'.$row['invoice'].'</td>
+                                        <td align="center">'.$row['po'].'</td>
+                                        <td align="center">'.$row['apt'].'</td>
+                                        <td align="center">'.$row['unit'].'</td>
+                                        <td align="center">'.$row['size'].'</td>
+                                        <td align="center">'.$row['price'].'</td>
+                                        <td align="center">'.$row['description'].'</td>
+                                        <td align="center">'.$row['date'].'</td>
+                                        <td align="center"><input type="submit" value="Send" onclick="location.href=\'assign.php\'"></input></td>
+                                    </tr>
+                                </tbody>';
+                        }
                     echo '</table>';
                     } else {
-                        $sql = 'SELECT * FROM SubWorksheet ORDER BY '.$order;
+                        $sql = 'SELECT * FROM SubWorksheet WHERE email =\"'.$_SESSION['email'].'\" ORDER BY '.$order;
                         $result = mysqli_query($conn, $sql);
                         echo '
-                        <table border="2" width="1000">
-                            <thead>
-                                <tr>
-                                    <td align="center"><b>Apt #</b></td>
-                                    <td align="center"><b>Unit #</b></td>
-                                    <td align="center"><b>Message</b></td>
-                                    <td align="center"><b>Comment</b></td>
-                                    <td align="center"><b>Date</b></td>
-                                </tr>
-                            </thead>';
-
-                            while($row = mysqli_fetch_array($result))
-                            {
-                                echo '
-                                    <tbody>
-                                        <tr>
-                                            <td align="center">'.$row['apt'].'</td>
-                                            <td align="center">'.$row['unit'].'</td>
-                                            <td align="center">'.$row['message'].'</td>
-                                            <td align="center">'.$row['comment'].'</td>
-                                            <td align="center">'.$row['date'].'</td>
-                                            <td align="center"><input type="submit" value="Send" onclick="location.href=\'assign.php\'"></input></td>
-                                        </tr>
-                                    </tbody>';
-                            }
+                            <table border="2" width="1000">
+                                <thead>
+                                    <tr>
+                                        <td align="center"><a href=\"?orderBy=apt\">Apt #</a></td>
+                                        <td align="center"><a href=\"?orderBy=unit\">Unit #</a></td>
+                                        <td align="center"><b>Message</b></td>
+                                        <td align="center"><b>Comment</b></td>
+                                        <td align="center"><a href=\"?orderBy=date\">Date</a></td>
+                                    </tr>
+                                </thead>';
+                        $orderBy = array('apt', 'unit', 'date');
+                        $order = 'date';
+                        if (isset($_GET['orderBy']) && in_array($_GET['orderBy'], $orderBy)) {
+                            $order = $_GET['orderBy'];
+                        }
+                        $sql = 'SELECT * FROM Worksheet ORDER BY '.$order;
+                        $result = mysqli_query($conn, $sql);
+                        while($row = mysqli_fetch_array($result))
+                        {
+                            echo '
+                                <tbody>
+                                    <tr>
+                                        <td align="center">'.$row['apt'].'</td>
+                                        <td align="center">'.$row['unit'].'</td>
+                                        <td align="center">'.$row['message'].'</td>
+                                        <td align="center">'.$row['comment'].'</td>
+                                        <td align="center">'.$row['date'].'</td>
+                                        <td align="center"><input type="submit" value="Send" onclick="location.href=\'assign.php\'"></input></td>
+                                    </tr>
+                                </tbody>';
+                        }
                     }
                     mysqli_close($conn);
                 ?>
 
-
                 </tbody>
                 <br><br>
-                <table border="2" width="1000">
-                    <thead>
-                        <tr>
-                            <td align="center"><b>Invoice #</b></th>
-                            <td align="center"><b>P.O. #</b></th>
-                            <td align="center"><b>Apt</b></th>
-                            <td align="center"><b>Unit #</b></th>
-                            <td align="center"><b>Size</b></th>
-                            <td align="center"><b>Price</b></th>
-                            <td align="center"><b>Descrition</b></th>  
-                            <td align="center"><b>Add</b></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <form action="worksheet_process.php" method="POST">
+                <?php if ($_SESSION['isadmin']): ?>
+                    <table border="2" width="1000">
+                        <thead>
                             <tr>
-                                <td align="center"><input type="text" name="invoice"></td>
-                                <td align="center"><input type="text" name="po"></td>
-                                <td align="center"><input type="text" name="apt"></td>
-                                <td align="center"><input type="text" name="unit"></td>
-                                <td align="center"><input type="text" name="size"></td>
-                                <td align="center"><input type="text" name="price"></td>
-                                <td align="center"><input type="text" name="description"></td>
-                                <td align="center"><input type="submit" value="OK"></td>
+                                <td align="center"><b>Invoice #</b></th>
+                                <td align="center"><b>P.O. #</b></th>
+                                <td align="center"><b>Apt</b></th>
+                                <td align="center"><b>Unit #</b></th>
+                                <td align="center"><b>Size</b></th>
+                                <td align="center"><b>Price</b></th>
+                                <td align="center"><b>Descrition</b></th>  
+                                <td align="center"><b>Add</b></th>
                             </tr>
-                        </form>
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            <form action="worksheet_process.php" method="POST">
+                                <tr>
+                                    <td align="center"><input type="text" name="invoice"></td>
+                                    <td align="center"><input type="text" name="po"></td>
+                                    <td align="center"><input type="text" name="apt"></td>
+                                    <td align="center"><input type="text" name="unit"></td>
+                                    <td align="center"><input type="text" name="size"></td>
+                                    <td align="center"><input type="text" name="price"></td>
+                                    <td align="center"><input type="text" name="description"></td>
+                                    <td align="center"><input type="submit" value="OK"></td>
+                                </tr>
+                            </form>
+                        </tbody>
+                    </table>
+                <?php  else: ?>
+                <?php endif; ?>
             </div>
         </div>
 
