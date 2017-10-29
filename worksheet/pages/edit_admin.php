82290252d94b27257d12feef6fd7fddcf7b9eeec
leepogii@gmail.com <leepogii@gmail.com>
2017-10-28 21:17:48 -0400

    Fix & Add functions

--- worksheet/pages/edit_admin.php
+++ worksheet/pages/edit_admin.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
+
     <?php 
-        // unset($_SESSION);
+        unset($_SESSION);
         include('./includes/head_tag.html'); 
 
         include('./includes/connection.php');
@@ -9,6 +10,7 @@
         if (isset($_GET['invoice_num'])) {
             $invoice = $_GET['invoice_num'];
             $invoice = str_replace("7C", "", $invoice);
+            $_SESSION['invoice'] = $invoice;
             $sql = "SELECT * FROM Worksheet WHERE invoice ='".$invoice."';";
             $result = mysqli_query($conn, $sql);
             $row = mysqli_fetch_array($result);
@@ -22,29 +24,6 @@
             $description = $row['description'];
         }
     ?>
-
-
-    <script type="text/javascript">
-        <?php 
-            $sql = "SELECT * FROM worksheet_description WHERE invoice = '$invoice';";
-            $result = $conn->query($sql);
-            if ($result->num_rows > 0) {
-                while($row = $result->fetch_assoc()) {
-                    $_SESSION['arr'][$_SESSION['i']][0] = $row['description'];
-                    $_SESSION['arr'][$_SESSION['i']][1] = $row['quantity'];
-                    $_SESSION['arr'][$_SESSION['i']][2] = $row['price'];
-                    $_SESSION['i']++;
-                }
-            }
-
-            echo '<script>
-                var a = 3;
-                alert(a);
-
-            </script>';
-        ?>
-
-    </script>
 
     <body>
         <div id="wrapper">
@@ -75,21 +54,21 @@
                                         </colgroup>
                                         <tr>
                                             <td align="right"><b><h5>Apt</h5></b></td>
-                                            <td align="left"><input type="text" class="form-control" name="apt" value="<?php echo isset($apt) ? $apt : '' ?>"></td>
+                                            <td align="left"><input type="text" class="form-control" name="apt" value="<?php echo isset($_SESSION['apt']) ? $_SESSION['apt'] : '' ?>"></td>
                                             <td align="right"><b><h5>Unit</h5></b></td>
-                                            <td align="left"><input type="text" class="form-control" name="unit" value="<?php echo isset($unit) ? $unit : '' ?>"></td>
+                                            <td align="left"><input type="text" class="form-control" name="unit" value="<?php echo isset($_SESSION['unit']) ? $_SESSION['unit'] : '' ?>"></td>
                                         </tr>
                                         <tr>
                                             <td align="right"><b><h5>P.O.</h5></b></td>
-                                            <td align="left"><input type="text" class="form-control" name="po" value="<?php echo isset($po) ? $po : '' ?>"></td>
+                                            <td align="left"><input type="text" class="form-control" name="po" value="<?php echo isset($_SESSION['po']) ? $_SESSION['po'] : '' ?>"></td>
                                             <td align="right"><b><h5>Co</h5></b></td>
-                                            <td align="left"><input type="text" class="form-control" id="company" name="company" value="<?php echo isset($company) ? $company : '' ?>"></td>
+                                            <td align="left"><input type="text" class="form-control" id="company" name="company" value="<?php echo isset($_SESSION['company']) ? $_SESSION['company'] : '' ?>"></td>
                                         </tr>
                                         <tr>
                                             <td align="right"><b><h5>Mgr</h5></b></td>
-                                            <td align="left"><input type="text" class="form-control" name="manager" value="<?php echo isset($manager) ? $manager : '' ?>"></td>
+                                            <td align="left"><input type="text" class="form-control" name="manager" value="<?php echo isset($_SESSION['manager']) ? $_SESSION['manager'] : '' ?>"></td>
                                             <td align="right"><b><h5>Size</h5></b></td>
-                                            <td align="left"><input type="text" class="form-control" name="size" value="<?php echo isset($size) ? $size : '' ?>"></td>
+                                            <td align="left"><input type="text" class="form-control" name="size" value="<?php echo isset($_SESSION['size']) ? $_SESSION['size'] : '' ?>"></td>
                                         </tr>
                                     </table>
                                     <?php
@@ -114,38 +93,27 @@
                                                         <td><input class="form-control" type="text" name="description" id="new_description"></td>
                                                         <td><input class="form-control" type="text" name="qty" id="new_quantity"></td>
                                                         <td><input class="form-control" type="text" name="price" id="new_price"></td>
-                                                        <td colspan="2"><input id="new_add" class="btn btn-primary btn-block add" type="button" onclick="add_row()" value="Add"></td>
+                                                        <td colspan="2"><input id="new_add" class="btn btn-primary btn-block add" type="button" onclick="add_row(1)" value="Add"></td>
                                                     </tr>
                                         ';
 
-                                        $num = sizeof($_SESSION['arr']);
-                                        if (isset($_SESSION['arr'])) {
-                                            for ($i = 0; $i < sizeof($_SESSION['arr']); $i++) {
-                                                if ($_SESSION['arr'][$i][0] !== null) {
-                                                    echo '<tr id="row'.$num.'"><td id="description_row'.$num.'"><div class="lineBreak">'.$_SESSION['arr'][$i][0].'</div></td>';
-                                                }
-                                                if ($_SESSION['arr'][$i][1] !== null) {
-                                                    echo '<td id="quantity_row'.$num.'"><div class="lineBreak">'.$_SESSION['arr'][$i][1].'</div></td>';
-                                                }
+                                        $sql = "SELECT * FROM worksheet_description WHERE invoice =".$_SESSION['invoice'].";";
+                                        $result = $conn->query($sql);
+                                        if ($result->num_rows > 0) {
+                                            $i = 0;
+                                            while($row = $result->fetch_assoc()) {
+                                                echo '<tr id="row'.$i.'"><td id="description_row'.$i.'"><div class="lineBreak">'.$row['description'].'</div></td>';
+                                                echo '<td id="quantity_row'.$i.'"><div class="lineBreak">'.$row['quantity'].'</div></td>';
 
-                                                if ($_SESSION['arr'][$i][2] !== null) {
-                                                    echo '<td id="price_row'.$num.'"><div class="lineBreak">'.$_SESSION['arr'][$i][2].'</div></td>';
-                                                }
-                                                if ($_SESSION['arr'][$i][0] !== null) {
-                                                    echo '
-                                                        <td><div class="btn-group"><button type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></button><ul class="dropdown-menu" role="menu"><li><a id="edit_button'.$num.'" class="edit" onclick="edit_row('.$num.')">Edit</a></li><li><a id="save_button'.$num.'" class="save" onclick="save_row('.$num.')">Save</a></li><li><a class="delete" onclick="delete_row('.$num.')">Delete</a></li></ul></div></td></tr>
-                                                    ';
-                                                }
-
-                                                // if ($_SESSION['arr'][$i][0] !== null) {
-                                                //     echo '<td align="center"><button class="btn btn-primary btn-block" type="button" onclick="location.href=\'edit_invoice_detail.php?description='.$_SESSION['arr'][$i][0].' &qty='.$_SESSION['arr'][$i][1].' &price='.$_SESSION['arr'][$i][2].' &index='.$i.'\'">Edit</button></td>';
-                                                // }
-                                                // if ($_SESSION['arr'][$i][0] !== null) {
-                                                //     echo '<td align="center"><button class="btn btn-primary btn-block" type="button" onclick="\'edit_invoice_detail.php?index_deleted='.$i.'\'">Delete</button></td></tr>';
-                                                // }
+                                                echo '<td id="price_row'.$i.'"><div class="lineBreak">'.$row['price'].'</div></td>';
+                                                echo '
+                                                    <td><div class="btn-group"><button type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></button><ul class="dropdown-menu" role="menu"><li><a id="edit_button'.$i.'" class="edit" onclick="edit_row('.$i.')">Edit</a></li><li><a id="save_button'.$i.'" class="save" onclick="save_row('.$i.')">Save</a></li><li><a class="delete" onclick="delete_row('.$i.')">Delete</a></li></ul></div></td></tr>
+                                                ';
+                                                $i++;
                                             }
-                                            $num++;
                                         }
+                                                    
+
 
                                         echo '
                                                 </tbody>
@@ -158,7 +126,7 @@
                                     <div class="row">
                                         <div class="col-sm-offset-5 col-sm-2 text-center">
                                             <div class="text-center btn-group">
-                                                <button class="btn btn-primary" type="submit" onclick="pass_data(7, 'edit_process.php', 3)">Save</button>
+                                                <button class="btn btn-primary" type="button" onclick="pass_data(7, 'edit_process.php', 3)">Save</button>
                                                 <button class="btn btn-primary" type="button" onclick="location.href='worksheet.php'">Back</button>
                                             </div>  
                                         </div>
